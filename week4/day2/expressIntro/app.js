ace16413ac589108535c0855368fb4e666b26ec0
dmquinn <danielmurphyberling@gmail.com>
2022-11-08 09:04:15 +0100

    express lesson updated

---
+++ week4/day2/expressIntro/app.js
@@ -0,0 +1,20 @@
+// Require Express
+const express = require("express");
+
+// Express server handling requests and responses
+const app = express();
+// Make everything inside of public/ available
+app.use(express.static("public"));
+
+// our first Route:
+app.get("/home", (request, response, next) =>
+  response.sendFile(__dirname + "/views/homepage.html")
+);
+
+// cat route:
+app.get("/cat", (request, response, next) =>
+  response.sendFile(__dirname + "/views/catpage.html")
+);
+
+// Server Started
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
